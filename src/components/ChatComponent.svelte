6a9fc2182b7c0a7060a53f90e8b89ab20e98fa77
Huang Jing <eatgrass@live.cn>
2023-12-27 13:30:19 +0800

    obsidian icon

--- src/components/ChatComponent.svelte
+++ src/components/ChatComponent.svelte
@@ -3,6 +3,7 @@ import { tick, onMount } from 'svelte'
 import type { GeminiChat } from 'GeminiService'
 import { MarkdownRenderer, type ItemView, type App } from 'obsidian'
 import GeminiIcon from './GeminiIcon.svelte'
+import { nanoid } from 'nanoid'
 
 export let gemini: GeminiChat
 export let view: ItemView
@@ -197,16 +198,204 @@ const enter = (e: KeyboardEvent) => {
                 <div class="gemini-conv-line">
                     <div class="gemini-conv-icon">
                         {#if conv.role == 'user'}
+                            {@const bid = nanoid()}
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                x="0px"
-                                y="0px"
-                                viewBox="0 0 64 64"
-                                style="stroke:var(--color-purple)"
+                                fill="none"
+                                viewBox="0 0 512 512"
                             >
-                                <path
-                                    d="M50.707,16.293l-13-13c-0.312-0.311-0.788-0.382-1.175-0.177l-17,9c-0.2,0.105-0.357,0.276-0.448,0.483l-7,16	c-0.105,0.24-0.111,0.514-0.018,0.76l10,26c0.114,0.295,0.36,0.52,0.666,0.604l17.974,5C40.795,60.988,40.885,61,40.974,61	c0.384,0,0.743-0.222,0.909-0.584l5.026-11c0.043-0.093,0.07-0.191,0.083-0.292l4-32C51.03,16.817,50.926,16.511,50.707,16.293z M39.307,58.498L24.071,54.26l2.543-27.973L39.307,58.498z M26.155,20.431l-4.709-7.064l0,0L36,5.661v4.925L26.155,20.431z M38,6.414l7.328,7.328L38,10.36V6.414z M14.081,29.024l6.102-13.947l4.789,7.184l-2.579,28.374L14.081,29.024z M45.026,48.724	l-3.985,8.721l-13.87-35.202L37.214,12.2l11.703,5.401l0,0L45.026,48.724z"
-                                ></path>
+                                <radialGradient
+                                    id="{bid}_bottom_left"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(-59 -225 150 -39 161.4 470)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity=".4"
+                                    />
+                                    <stop offset="1" stop-opacity=".1" />
+                                </radialGradient>
+                                <radialGradient
+                                    id="{bid}_top-right"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(50 -379 280 37 360 374.2)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity=".6"
+                                    />
+                                    <stop
+                                        offset="1"
+                                        stop-color="#fff"
+                                        stop-opacity=".1"
+                                    />
+                                </radialGradient>
+                                <radialGradient
+                                    id="{bid}_top-left"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(69 -319 218 47 175.4 307)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity=".8"
+                                    />
+                                    <stop
+                                        offset="1"
+                                        stop-color="#fff"
+                                        stop-opacity=".4"
+                                    />
+                                </radialGradient>
+                                <radialGradient
+                                    id="{bid}_bottom-right"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(-96 -163 187 -111 335.3 512.2)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity=".3"
+                                    />
+                                    <stop offset="1" stop-opacity=".3" />
+                                </radialGradient>
+                                <radialGradient
+                                    id="{bid}_top-edge"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(-36 166 -112 -24 310 128.2)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity="0"
+                                    />
+                                    <stop
+                                        offset="1"
+                                        stop-color="#fff"
+                                        stop-opacity=".2"
+                                    />
+                                </radialGradient>
+                                <radialGradient
+                                    id="{bid}_left-edge"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(88 89 -190 187 111 220.2)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity=".2"
+                                    />
+                                    <stop
+                                        offset="1"
+                                        stop-color="#fff"
+                                        stop-opacity=".4"
+                                    />
+                                </radialGradient>
+                                <radialGradient
+                                    id="{bid}_bottom-edge"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(9 130 -276 20 215 284)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity=".2"
+                                    />
+                                    <stop
+                                        offset="1"
+                                        stop-color="#fff"
+                                        stop-opacity=".3"
+                                    />
+                                </radialGradient>
+                                <radialGradient
+                                    id="{bid}_middle-edge"
+                                    cx="0"
+                                    cy="0"
+                                    gradientTransform="matrix(-198 -104 327 -623 400 399.2)"
+                                    gradientUnits="userSpaceOnUse"
+                                    r="1"
+                                >
+                                    <stop
+                                        offset="0"
+                                        stop-color="#fff"
+                                        stop-opacity=".2"
+                                    />
+                                    <stop
+                                        offset=".5"
+                                        stop-color="#fff"
+                                        stop-opacity=".2"
+                                    />
+                                    <stop
+                                        offset="1"
+                                        stop-color="#fff"
+                                        stop-opacity=".3"
+                                    />
+                                </radialGradient>
+                                <clipPath id="{bid}_clip">
+                                    <path d="M.2.2h512v512H.2z" />
+                                </clipPath>
+                                <g clip-path="url(#{bid}_clip)">
+                                    <path
+                                        d="M382.3 475.6c-3.1 23.4-26 41.6-48.7 35.3-32.4-8.9-69.9-22.8-103.6-25.4l-51.7-4a34 34 0 0 1-22-10.2l-89-91.7a34 34 0 0 1-6.7-37.7s55-121 57.1-127.3c2-6.3 9.6-61.2 14-90.6 1.2-7.9 5-15 11-20.3L248 8.9a34.1 34.1 0 0 1 49.6 4.3L386 125.6a37 37 0 0 1 7.6 22.4c0 21.3 1.8 65 13.6 93.2 11.5 27.3 32.5 57 43.5 71.5a17.3 17.3 0 0 1 1.3 19.2 1494 1494 0 0 1-44.8 70.6c-15 22.3-21.9 49.9-25 73.1z"
+                                        fill="#6c31e3"
+                                    />
+                                    <path
+                                        d="M165.9 478.3c41.4-84 40.2-144.2 22.6-187-16.2-39.6-46.3-64.5-70-80-.6 2.3-1.3 4.4-2.2 6.5L60.6 342a34 34 0 0 0 6.6 37.7l89.1 91.7a34 34 0 0 0 9.6 7z"
+                                        fill="url(#{bid}_bottom-left)"
+                                    />
+                                    <path
+                                        d="M278.4 307.8c11.2 1.2 22.2 3.6 32.8 7.6 34 12.7 65 41.2 90.5 96.3 1.8-3.1 3.6-6.2 5.6-9.2a1536 1536 0 0 0 44.8-70.6 17 17 0 0 0-1.3-19.2c-11-14.6-32-44.2-43.5-71.5-11.8-28.2-13.5-72-13.6-93.2 0-8.1-2.6-16-7.6-22.4L297.6 13.2a34 34 0 0 0-1.5-1.7 96 96 0 0 1 2 54 198.3 198.3 0 0 1-17.6 41.3l-7.2 14.2a171 171 0 0 0-19.4 71c-1.2 29.4 4.8 66.4 24.5 115.8z"
+                                        fill="url(#{bid}_top-right)"
+                                    />
+                                    <path
+                                        d="M278.4 307.8c-19.7-49.4-25.8-86.4-24.5-115.9a171 171 0 0 1 19.4-71c2.3-4.8 4.8-9.5 7.2-14.1 7.1-13.9 14-27 17.6-41.4a96 96 0 0 0-2-54A34.1 34.1 0 0 0 248 9l-105.4 94.8a34.1 34.1 0 0 0-10.9 20.3l-12.8 85-.5 2.3c23.8 15.5 54 40.4 70.1 80a147 147 0 0 1 7.8 24.8c28-6.8 55.7-11 82.1-8.3z"
+                                        fill="url(#{bid}_top-left)"
+                                    />
+                                    <path
+                                        d="M333.6 511c22.7 6.2 45.6-12 48.7-35.4a187 187 0 0 1 19.4-63.9c-25.6-55-56.5-83.6-90.4-96.3-36-13.4-75.2-9-115 .7 8.9 40.4 3.6 93.3-30.4 162.2 4 1.8 8.1 3 12.5 3.3 0 0 24.4 2 53.6 4.1 29 2 72.4 17.1 101.6 25.2z"
+                                        fill="url(#{bid}_bottom-right)"
+                                    />
+                                    <g clip-rule="evenodd" fill-rule="evenodd">
+                                        <path
+                                            d="M254.1 190c-1.3 29.2 2.4 62.8 22.1 112.1l-6.2-.5c-17.7-51.5-21.5-78-20.2-107.6a174.7 174.7 0 0 1 20.4-72c2.4-4.9 8-14.1 10.5-18.8 7.1-13.7 11.9-21 16-33.6 5.7-17.5 4.5-25.9 3.8-34.1 4.6 29.9-12.7 56-25.7 82.4a177.1 177.1 0 0 0-20.7 72z"
+                                            fill="url(#{bid}_top-edge)"
+                                        />
+                                        <path
+                                            d="M194.3 293.4c2.4 5.4 4.6 9.8 6 16.5L195 311c-2.1-7.8-3.8-13.4-6.8-20-17.8-42-46.3-63.6-69.7-79.5 28.2 15.2 57.2 39 75.7 81.9z"
+                                            fill="url(#{bid}_left-edge)"
+                                        />
+                                        <path
+                                            d="M200.6 315.1c9.8 46-1.2 104.2-33.6 160.9 27.1-56.2 40.2-110.1 29.3-160z"
+                                            fill="url(#{bid}_bottom-edge)"
+                                        />
+                                        <path
+                                            d="M312.5 311c53.1 19.9 73.6 63.6 88.9 100-19-38.1-45.2-80.3-90.8-96-34.8-11.8-64.1-10.4-114.3 1l-1.1-5c53.2-12.1 81-13.5 117.3 0z"
+                                            fill="url(#{bid}_middle-edge)"
+                                        />
+                                    </g>
+                                </g>
                             </svg>
                         {:else}
                             <GeminiIcon width={null} height={null} />
